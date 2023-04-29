14956b1b9e4a203c48cb7e531c5b241fdc944993
Neal Stanard <nstanard@users.noreply.github.com>
2023-04-29 12:45:04 -0500

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('@types/prettier').Config}
+ */
+module.exports = {
+    tabWidth: 4,
+    trailingComma: 'all',
+    bracketSameLine: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+};
